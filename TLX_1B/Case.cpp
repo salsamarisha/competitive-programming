85f330596932a14c2ee6387530c1e5fee8df00d0
Marisha Salsabila <salsamarisha98@gmail.com>
2017-10-26 15:11:34 +0700

    Add files via upload

---
+++ TLX_1B/Case.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin>>a;
+		switch(a) {
+			case 1 ... 9:
+				cout<<"satuan"<<endl;
+				break;
+			case 10 ... 99:
+				cout<<"puluhan"<<endl;
+				break;
+			case 100 ... 999:
+				cout<<"ratusan"<<endl;
+				break;
+			case 1000 ... 9999:
+				cout<<"ribuan"<<endl;
+				break;
+			case 10000 ... 99999:
+				cout<<"puluhribuan"<<endl;
+				break;}
+		return 0;
+}
---
+++ TLX_1B/If Then, Multi Condition.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+		
+		if (n>0&&n%2==0)
+			{cout<<n<<endl;
+			}
+	
+	return 0;
+}
